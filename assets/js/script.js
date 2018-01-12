d28f3a4d33f5520187c9dc5bf65403e283f9b20f
malcodeman <malcodeman@users.noreply.github.com>
2018-01-12 01:17:29 +0100

    Social media icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let images = document.getElementsByClassName("gallery-item");
 
 for (let i = 0; i < images.length; ++i) {
-    images[i].src = "https://source.unsplash.com/random?" + i;
+    //images[i].src = "https://source.unsplash.com/random?" + i;
 }
